923e692a3171f7598925d291d81a02eb86688a5f
Venkatesh N P <venki21122@gmail.com>
2022-10-25 12:52:59 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  use_doorkeeper do
+    skip_controllers :authorizations, :applications, :authorized_applications
+  end
+end
---
+++ db/migrate/20221025061510_create_bookmarks.rb
@@ -0,0 +1,10 @@
+class CreateBookmarks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bookmarks do |t|
+      t.string :title
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
